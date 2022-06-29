4210ed2eb5e4adae8c01a8f39533820e45eb681e
TaftSound <mattmurphytattoos@gmail.com>
2022-06-29 11:08:14 -0700

    Adjust button colors and shading contrast

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// Need a placeholder for currentValue
+// Need a placeholder for nextValue
+// Need checks in place to know whether to store a number in currentValue or nextValue
+// If no operator has been selected, store new number entry in currentValue,
+    // else store in nextValue
+// Need a placeholder for current operator
+// What do you do if an operator has already been entered?
