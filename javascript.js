24547408da8f717bb664004ffa50b036c8baac3b
TaftSound <mattmurphytattoos@gmail.com>
2022-07-01 15:36:25 -0700

    Remove unused code

--- javascript.js
+++ javascript.js
@@ -1,20 +1,3 @@
-// Need a placeholder for currentValue
-// Need a placeholder for nextValue
-// Need checks in place to know whether to store a number in currentValue or nextValue
-// If no operator has been selected, store new number entry in currentValue,
-    // else store in nextValue
-// Need a placeholder for current operator
-// What do you do if an operator has already been entered?
-    // if last keypress was also an operator, switch current operator
-// create a container to store/execute the currentOperation
-    // when equal key is pressed, perform currentOperation, then store the resulting
-    // value in the currentValue variable, then empty the currentOperation container,
-    // the currentOperator container, and the nextValue container
-// when AC button is pressed, the display and all variable should be emptied
-// when the -/+ button is pressed
-
-// When inputting numbers and displaying results, have an if statement that checks
-// length and rounds numbers or uses scientific notation
 const decimalButton = document.getElementById('decimal');
 const zeroButton = document.getElementById('zero');
 const oneButton = document.getElementById('one');
